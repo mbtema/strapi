233482e0d7a98d7f3a2bbd63f26f70b3bd3e1079
tēma <akrsdmdn@icloud.com>
2026-09-03 16:53:34 +0500

    Create barcode-extractor.js

---
+++ userscripts/barcode-extractor.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         barcode-extractor
+// @version      1.1
+// @description  Копирует barcode с карточки товара
+// @match        http://10.10.3.80:1337/admin/*
+// @grant        GM_setClipboard
+// ==/UserScript==
+
+(() => {
+  'use strict';
+
+  document.addEventListener('keydown', (event) => {
+    if (event.ctrlKey && event.code === 'KeyB') {
+
+      const input =
+        document.querySelector('input[name="barcode"]') ||
+        document.querySelector('input[id*="barcode"]');
+
+      if (!input) {
+        console.warn('Strapi Helper: поле barcode не найдено');
+        return;
+      }
+
+      const value = input.value;
+
+      if (!value) {
+        console.warn('Strapi Helper: barcode пустой');
+        return;
+      }
+
+      GM_setClipboard(value);
+
+      console.log(`Strapi Helper: скопировано → ${value}`);
+    }
+  });
+
+  console.log('Strapi Helper запущен');
+})();
