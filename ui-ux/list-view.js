941de41257c4c0cd20be0d826d34fe06def52c20
tēma <akrsdmdn@icloud.com>
2026-09-08 15:42:24 +0500

    remove list-view extension

--- ui-ux/list-view.js
+++
@@ -1,9 +0,0 @@
-// ==StrapiExtension==
-// @name         list-view
-// @version      1.1.2
-// @description  Заготовка для кастомизации List View Content Manager
-// ==/StrapiExtension==
-
-(function () {
-    'use strict';
-})();
